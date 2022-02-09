6b4e0b7089223a4ec422aa66922284c680b95821
zidbon <zidbon@hotmail.com>
2022-02-09 10:19:42 -0800

    changed database to postgresql

--- whatchareadn/whatchareadn/settings.py
+++ whatchareadn/whatchareadn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,13 @@ WSGI_APPLICATION = 'whatchareadn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+from dotenv import load_dotenv
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'book',
+        'USER': 'bill',
+        'PASSWORD': os.environ.get('PASSWORD'),
     }
 }
 
