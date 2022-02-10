092f751b17908309d50435aa05ad3bbd7e9a456e
zidbon <zidbon@hotmail.com>
2022-02-09 22:47:05 -0800

    connected to google books api

--- whatchareadn/main/views.py
+++ whatchareadn/main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+import requests
+import json
 
 # Create your views here.
 class Index(View):
 
     def get(self, request):
 
+        search = "one piece"
+        r = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + search, params=request.GET)
+        print(r.text)
+        stuff = json.loads(r.text)
+
         context = {
+                'stuff': stuff,
         }
 
         return render(request, 'main/index.html', context)
